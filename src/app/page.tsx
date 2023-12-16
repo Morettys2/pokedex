95dcd5247ef6be00d0b3d054992e2a62232fe35e
Joao Pedro Moretti <m0rettyy20@gmail.com>
2023-12-16 12:42:23 -0300

    Atualizaçao na interface grafica

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,27 +19,37 @@ export default function Home() {
   }, [offset]);
 
   return (
-    <div className="container mx-auto my-8">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {pokemons.map((pokemon) => (
-          <PokemonCard key={pokemon.name} pokename={pokemon.name} />
-        ))}
-      </div>
-      <div className="flex justify-between mt-4">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => setOffset(offset + 10)}
-        >
-          Next
-        </button>
-        {offset !== 0 && (
+    <div className="bg-gradient-to-b from-blue-700 via-blue-300 to-white min-h-screen p-8">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {pokemons.map((pokemon, index) => (
+            <div key={pokemon.name} className="bg-white p-4 rounded-lg shadow-md">
+              <p className="text-2xl font-bold mb-2">#{index + 1}</p>
+              <PokemonCard pokename={pokemon.name} />
+              {pokemon.types && (
+                <p className="text-gray-600 mt-2">
+                  Type: {pokemon.types.map((type) => type.type.name).join(', ')}
+                </p>
+              )}
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-between mt-8">
           <button
-            className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => setOffset(Math.max(offset - 10, 0))}
+            className="bg-yellow-400 hover:bg-yellow-500 text-blue-900 font-bold py-2 px-6 rounded-full transition duration-300 ease-in-out"
+            onClick={() => setOffset(offset + 10)}
           >
-            Back
+            Next
           </button>
-        )}
+          {offset !== 0 && (
+            <button
+              className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-6 rounded-full transition duration-300 ease-in-out"
+              onClick={() => setOffset(Math.max(offset - 10, 0))}
+            >
+              Back
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
